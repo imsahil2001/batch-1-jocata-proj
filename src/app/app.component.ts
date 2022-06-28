9537cfbdd14fbd51f07f3fb52a748908af13116b
imsahil2001 <sahilkkr56@gmail.com>
2022-06-28 16:40:48 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,8 +19,10 @@ export class AppComponent implements OnInit {
       Validators.required,
     ]),
     PhoneNumber: new FormControl('', [
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
       Validators.required,
-      Validators.pattern('/^[0-9-]*$/'),
+      Validators.minLength(10),
+      Validators.maxLength(10),
     ]),
     CAddress: new FormControl('', [Validators.required]),
     CAddress1: new FormControl('', [Validators.required]),
