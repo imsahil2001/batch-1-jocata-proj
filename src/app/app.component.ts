4f1cafcd69d94e5989c68036b162864cb19c52c9
imsahil2001 <sahilkkr56@gmail.com>
2022-06-28 16:12:01 +0530

    validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,174 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   title = 'official_project';
-  myReactiveForm: any;
-
-  ngOnInit() {
-    this.myReactiveForm = new FormGroup({
-      name: new FormControl('', [
-        Validators.pattern('[A-Za-z]{1,32}'),
-        Validators.required,
-      ]),
-      email: new FormControl(null),
-    });
-  }
+  ngOnInit() {}
+  myReactiveForm: any = new FormGroup({
+    FirstName: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    LastName: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    PhoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.pattern('/^[0-9-]*$/'),
+    ]),
+    CAddress: new FormControl('', [Validators.required]),
+    CAddress1: new FormControl('', [Validators.required]),
+    PAddress: new FormControl('', [Validators.required]),
+    PAddress1: new FormControl('', [Validators.required]),
+    PinCode: new FormControl('', [
+      Validators.pattern('^[1-9][0-9]{5}$'),
+      Validators.required,
+    ]),
+    Email: new FormControl('', [Validators.required, Validators.email]),
+    DOB: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(2|1){1}[0-9]{3}.[0-9]{2}.[0-9]{2}.*$'),
+    ]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    phone: new FormControl(null, [
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+    country: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z]{1,}$'),
+    ]),
+    passport: new FormControl('', [
+      Validators.pattern('^[A-PR-WYa-pr-wy][1-9]\\d\\s?\\d{4}[1-9]$'),
+    ]),
+    issueddate: new FormControl(null, [Validators.required]),
+    expirationdate: new FormControl(null, [Validators.required]),
+    issuedby: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('^[a-zA-Z]{1,}$'),
+    ]),
+    pan: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('[A-Z]{5}[0-9]{4}[A-Z]{1}'),
+    ]),
+    panname: new FormControl(null, [
+      Validators.pattern('^[a-zA-Z][a-zA-Z\\s]+$'),
+    ]),
+    aadhar: new FormControl(null, [
+      Validators.required,
+      Validators.pattern('^[2-9]{1}[0-9]{3}\\s[0-9]{4}\\s[0-9]{4}$'),
+    ]),
+    aadharname: new FormControl(null, [
+      Validators.pattern('^[a-zA-Z][a-zA-Z\\s]+$'),
+    ]),
+    fromyr: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{1,4}'),
+    ]),
+    toyr: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[0-9]{1,4}'),
+    ]),
+    designation: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[A-Za-z]{1,}'),
+    ]),
+    location: new FormControl('', [
+      Validators.required,
+      Validators.pattern('[A-Za-z]{1,}'),
+    ]),
+    qualification: new FormControl('', [
+      Validators.pattern('[a-zA-Z.{}";_^%$#!~@,-]{1,32}'),
+      Validators.required,
+    ]),
+    major: new FormControl('', [
+      Validators.pattern('[a-zA-Z.{}";_^%$#!~@,-]{1,32}'),
+      Validators.required,
+    ]),
+    Membership: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    OrgMembership: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    date: new FormControl('', [
+      Validators.pattern('^(2|1){1}[0-9]{3}.[0-9]{2}.[0-9]{2}.*$'),
+      Validators.required,
+    ]),
+    honour: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    honouraward: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    grantor: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    year: new FormControl('', [
+      Validators.pattern('[0-9]{1,4}'),
+      Validators.required,
+    ]),
+    state: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    institute: new FormControl('', [Validators.required]),
+    yearReceived: new FormControl('', [
+      Validators.pattern('[0-9]{1,4}'),
+      Validators.required,
+    ]),
+    emergencyName: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]),
+    relation: new FormControl('', [
+      Validators.pattern('[A-Za-z]{1,32}'),
+      Validators.required,
+    ]), //
+    phone1: new FormControl('', [
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+    phone2: new FormControl('', [
+      Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(10),
+    ]),
+    address1: new FormControl('', Validators.required),
+    address2: new FormControl('', Validators.required),
+    pin: new FormControl('', [
+      Validators.pattern('[0-9]{6}'),
+      Validators.required,
+    ]),
+    gender: new FormControl('', Validators.required),
+    BloodGrp: new FormControl('', [
+      Validators.pattern('^(A|B|AB|O)[+-]$'),
+      Validators.required,
+    ]),
+    inquiry: new FormControl('', [
+      Validators.pattern('^(?!d+$)(?:[a-zA-Z][a-zA-Z @&$,]*)?$'),
+      Validators.required,
+    ]),
+    Healthinfo: new FormControl('', [
+      Validators.pattern('^(?!d+$)(?:[a-zA-Z][a-zA-Z @&$,]*)?$'),
+      Validators.required,
+    ]),
+    illness: new FormControl('', [
+      Validators.pattern('^(?!d+$)(?:[a-zA-Z][a-zA-Z @&$,]*)?$'),
+      Validators.required,
+    ]),
+    disability: new FormControl('', [
+      Validators.pattern('^(?!d+$)(?:[a-zA-Z][a-zA-Z @&$,]*)?$'),
+      Validators.required,
+    ]),
+  });
 }
